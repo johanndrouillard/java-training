7f3185b4f1ac6876cc31b39bbbb3c2b7611c9531
johanndrouillard <johanndrouillard@gmail.com>
2019-10-07 17:15:38 +0200

    exercice un

--- src/exercices/PrimitiveTraining.java
+++ src/exercices/PrimitiveTraining.java
@@ -2,7 +2,23 @@ public class PrimitiveTraining {
 
     // TODO 1 : Change the method 'integer' to return the expected result : int 42
     public static int integer() {
-        return 0;
+        return 42;
+    }
+
+    public static String str() {
+        return "Hello";
+    }
+    
+    public static double real() {
+        return 3.14;
+    } 
+
+    public static boolean bool() {
+        return false;
+    }
+
+    public static void nothing() {
+        
     }
 
     // TODO 2 : Create a method 'str', result expected : String "Hello"
